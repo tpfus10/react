b41e114e2527fa4c16ad2152694874817b2de229
tpfus10 <tpfusdl1a@naver.com>
2024-03-08 18:02:38 +0900

    0308

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
+import logo from './logo.svg';//이미지도 모듈처럼 가져옴
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+function App() { //App모듈 = App함수
+  return (//App이 리턴하는 태그
+      <> 
+  <div className="App">
       <header className="App-header">
+        <p>Welcome, seryeon!</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome, seryeon!
-        </p>
       </header>
     </div>
+      </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));//html의 div를 가져와서 돔트리의 루트로 만듦
+root.render(//root에 앱을 끼워넣는다는 의미(html 태그를 리턴함->html에 직접 작성하지 않은 태그들이 들어감)
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -14,4 +14,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(console.log);
